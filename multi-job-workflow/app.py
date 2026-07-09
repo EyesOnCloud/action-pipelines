9a5bc1f553120019b0e1c729478de05baee5fbeb
EyesOnCloud <naushad.p.tech@gmail.com>
2026-07-09 10:02:35 +0530

    Delete multi-job-workflow directory

--- multi-job-workflow/app.py
+++
@@ -1,5 +0,0 @@
-def greet():
-    return "Hello from EyesOnCloud App!"
-
-if __name__ == "__main__":
-    print(greet()) 
--- multi-job-workflow/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import greet
-
-def test_greet():
-    assert greet() == "Hello from EyesOnCloud App!"
-
-test_greet()
-print("All tests passed!")
